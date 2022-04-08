6529ddf4f54094b458bde03c54837b1e4dc82330
Jandreumc <jandreumc@gmail.com>
2022-04-08 17:19:54 +0200

    cambio titulo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
     public function comparativa()
     {
        
-        $tituloh1='Comparativa Ventas Producto Z 2020-2021';
+        $tituloh1='Ventas Producto Z 2020-2021';
         $datagrafico1=[12,23,5,9,7,10,4,9,14,10,3,6];
         $datagrafico2=[ 10,5,21,8,7,11,6,5,12,9,3,2];
 
